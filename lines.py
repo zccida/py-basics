fef9427e648fa29757a0fc50ab5d3f9752bbd38c
zccida <fdh3130@gmail.com>
2025-05-18 01:14:40 +0900

    Update lines.py

--- lines.py
+++ lines.py
@@ -15,12 +15,28 @@ def main():
     elif len(sys.argv) > 2:
         sys.exit("Too many command-line arguments")
     elif len(sys.argv) == 2:
-        try:
-            with open(sys.argv[1], "r") as file:
-                print(file)
-        except FileNotFoundError:
-            sys.exit("File does not exist")
+        if sys.argv[1].endswith('.py'):
+            try:
+                with open(sys.argv[1], "r") as file:
+                    count = read_file(sys.argv[1])
+                    print(count)
+            except FileNotFoundError:
+                sys.exit("File does not exist")
+        else:
+            sys.exit("Not a Python file")
 
 
+def read_file(file_name):
+    counter = 0
+    with open(file_name, 'r') as file:
+        for line in file:
+            line = line.strip()
+
+            if len(line) == 0 or line.startswith('#'):
+                continue
+            else:
+                counter = counter + 1
+
+    return counter
 if __name__ == "__main__":
     main()
